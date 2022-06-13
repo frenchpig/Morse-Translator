5a9b2b9dc9781f56c5780da10a8c413c6f557567
frenchpig <auspont.javier@gmail.com>
2022-06-13 00:25:41 -0400

    Update morsetranslator.py

--- morsetranslator.py
+++ morsetranslator.py
@@ -1,118 +1,119 @@
 import os
 os.system('cls')
 
-ejecutarsys = "s"
+ejecutarsys = "y"
 
-while ejecutarsys == "s":
-    listamorse = []
-    print("--- Traductor codigo morse ---")
+while ejecutarsys == "y":
+    morselist = []
+    print("--- Morse Code Translator ---")
 
-    palabra = input("Ingrese una palabra: ")
-    if len(palabra) == 0:
-        errorpalabra = True
-        while errorpalabra:
-            print("Error: Debe ingresar una palabra")
-            palabra = input("Ingrese una palabra: ")
-            if len(palabra)>0:
-                errorpalabra = False
+    word = input("Enter a word: ")
+    if len(word) == 0:
+        worderror = True
+        while worderror:
+            print("Error: You must enter a word")
+            word = input("Enter a word: ")
+            if len(word)>0:
+                worderror = False
 
-    palabra = palabra.lower()
+    word = word.lower()
 
     os.system('cls')
 
-    for i in range(len(palabra)):
-        if palabra[i] == "a":
-            listamorse.append(".-")
-        elif palabra[i] == "b":
-            listamorse.append("-...")
-        elif palabra[i] == "c":
-            listamorse.append("-.-.")
-        elif palabra[i] == "d":
-            listamorse.append("-..")
-        elif palabra[i] == "e":
-            listamorse.append(".")
-        elif palabra[i] == "f":
-            listamorse.append("..-.")
-        elif palabra[i] == "g":
-            listamorse.append("--.")
-        elif palabra[i] == "h":
-            listamorse.append("....")
-        elif palabra[i] == "i":
-            listamorse.append("..")
-        elif palabra[i] == "j":
-            listamorse.append(".---")
-        elif palabra[i] == "k":
-            listamorse.append("-.-")
-        elif palabra[i] == "l":
-            listamorse.append(".-..")
-        elif palabra[i] == "m":
-            listamorse.append("--")
-        elif palabra[i] == "n":
-            listamorse.append("-.")
-        elif palabra[i] == "o":
-            listamorse.append("---")
-        elif palabra[i] == "p":
-            listamorse.append(".--.")
-        elif palabra[i] == "q":
-            listamorse.append("--.-")
-        elif palabra[i] == "r":
-            listamorse.append(".-.")
-        elif palabra[i] == "s":
-            listamorse.append("...")
-        elif palabra[i] == "t":
-            listamorse.append("-")
-        elif palabra[i] == "u":
-            listamorse.append("..-")
-        elif palabra[i] == "v":
-            listamorse.append("...-")
-        elif palabra[i] == "w":
-            listamorse.append(".--")
-        elif palabra[i] == "x":
-            listamorse.append("-..-")
-        elif palabra[i] == "y":
-            listamorse.append("-.--")
-        elif palabra[i] == "z":
-            listamorse.append("--..")
-        elif palabra[i] == "1":
-            listamorse.append(".----")
-        elif palabra[i] == "2":
-            listamorse.append("..---")
-        elif palabra[i] == "3":
-            listamorse.append("...--")
-        elif palabra[i] == "4":
-            listamorse.append("....-")
-        elif palabra[i] == "5":
-            listamorse.append(".....")
-        elif palabra[i] == "6":
-            listamorse.append("-....")
-        elif palabra[i] == "7":
-            listamorse.append("--...")
-        elif palabra[i] == "8":
-            listamorse.append("---..")
-        elif palabra[i] == "9":
-            listamorse.append("----.")
-        elif palabra[i] == "0":
-            listamorse.append("-----")
-        elif palabra[i] == ".":
-            listamorse.append(".-.-.-")
-        elif palabra[i] == ",":
-            listamorse.append("--..--")
-        elif palabra[i] == "?":
-            listamorse.append("..--..")
+    for i in range(len(word)):
+        if word[i] == "a":
+            morselist.append(".-")
+        elif word[i] == "b":
+            morselist.append("-...")
+        elif word[i] == "c":
+            morselist.append("-.-.")
+        elif word[i] == "d":
+            morselist.append("-..")
+        elif word[i] == "e":
+            morselist.append(".")
+        elif word[i] == "f":
+            morselist.append("..-.")
+        elif word[i] == "g":
+            morselist.append("--.")
+        elif word[i] == "h":
+            morselist.append("....")
+        elif word[i] == "i":
+            morselist.append("..")
+        elif word[i] == "j":
+            morselist.append(".---")
+        elif word[i] == "k":
+            morselist.append("-.-")
+        elif word[i] == "l":
+            morselist.append(".-..")
+        elif word[i] == "m":
+            morselist.append("--")
+        elif word[i] == "n":
+            morselist.append("-.")
+        elif word[i] == "o":
+            morselist.append("---")
+        elif word[i] == "p":
+            morselist.append(".--.")
+        elif word[i] == "q":
+            morselist.append("--.-")
+        elif word[i] == "r":
+            morselist.append(".-.")
+        elif word[i] == "s":
+            morselist.append("...")
+        elif word[i] == "t":
+            morselist.append("-")
+        elif word[i] == "u":
+            morselist.append("..-")
+        elif word[i] == "v":
+            morselist.append("...-")
+        elif word[i] == "w":
+            morselist.append(".--")
+        elif word[i] == "x":
+            morselist.append("-..-")
+        elif word[i] == "y":
+            morselist.append("-.--")
+        elif word[i] == "z":
+            morselist.append("--..")
+        elif word[i] == "1":
+            morselist.append(".----")
+        elif word[i] == "2":
+            morselist.append("..---")
+        elif word[i] == "3":
+            morselist.append("...--")
+        elif word[i] == "4":
+            morselist.append("....-")
+        elif word[i] == "5":
+            morselist.append(".....")
+        elif word[i] == "6":
+            morselist.append("-....")
+        elif word[i] == "7":
+            morselist.append("--...")
+        elif word[i] == "8":
+            morselist.append("---..")
+        elif word[i] == "9":
+            morselist.append("----.")
+        elif word[i] == "0":
+            morselist.append("-----")
+        elif word[i] == ".":
+            morselist.append(".-.-.-")
+        elif word[i] == ",":
+            morselist.append("--..--")
+        elif word[i] == "?":
+            morselist.append("..--..")
 
-    traduccion = ""
-    for i in range(len(listamorse)):
+    translation = ""
+    for i in range(len(morselist)):
         if i == 0:
-            traduccion = traduccion + listamorse[i]
+            translation = translation + morselist[i]
         else:
-            traduccion = traduccion + " / "
-            traduccion = traduccion + listamorse[i]
+            translation = translation + " / "
+            translation = translation + morselist[i]
 
 
-    print(f"--- Traduccion Finalizada ---\nPalabra ingresada: {palabra}\nTraduccion a morse: {traduccion}")
-    pausa = input("Presione Enter para continuar...")
+    print(f"--- Translation Finished ---\nEntered word: {word}\nMorse Translation: {translation}")
+    pausa = input("Press Enter to continue...")
     os.system('cls')
 
-    ejecutarsys = input("¿Desea traducir algo mas? s o n\n")
+    ejecutarsys = input("Do you want to translate something else? y o n\n")
     ejecutarsys = ejecutarsys.lower()
     os.system('cls')
+
